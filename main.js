0da52c3ace5ca8bfbaeacfe250ada406823a8584
caleybennett <56694583+caleybennett@users.noreply.github.com>
2019-10-22 19:38:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+//const cardsInPlay = [];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+//var cardTwo = cards[3];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
+//if (cardsInPlay.length === 2){
+	//if (cardsInPlay[0] === cardsInPlay[1]) {
+		//result = alert("You found a match!");
+	//} else {
+		//result = alert("Sorry, try again")
+	//}
+//console.log("User flipped " + cardOne);
+
