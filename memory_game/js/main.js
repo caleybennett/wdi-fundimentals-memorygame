d7e5e72b31fd573af4472d7d60a79eb3a03875a8
caleybennett <56694583+caleybennett@users.noreply.github.com>
2019-10-22 21:12:48 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,25 @@
-// console.log("Up and running!");
-// console.log("User flipped " + cardOne);
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2){
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+function flipCard(cardID) {
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch()
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		result = alert("You found a match!");
 	} else {
 		result = alert("Sorry, try again")
 	}
-}
-console.log("User flipped " + cardOne);
+	console.log("user flipped " + cards[cardID])
+
+	}
+flipCard(0)
+flipCard(2)
+
 
