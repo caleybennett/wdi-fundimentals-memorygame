08f641ec15541647809a36f199a8d968a3b2c41a
caleybennett <56694583+caleybennett@users.noreply.github.com>
2019-10-23 10:51:12 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png", 
+}
+];
 const cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,14 +31,16 @@ function checkForMatch() {
 }
 }
 function flipCard(cardID) {
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch()
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		result = alert("You found a match!");
 	} else {
 		result = alert("Sorry, try again")
 	}
-	console.log("user flipped " + cards[cardID])
+	console.log("user flipped " + cards[cardID].rank)
+	console.log(cards[cardID].cardImage)
+	console.log(cards[cardID].suit)
 
 	}
 flipCard(0)
