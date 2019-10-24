9d3fcb93e10ad0d22c5dd245f55d718b42916171
caleybennett <56694583+caleybennett@users.noreply.github.com>
2019-10-24 14:21:43 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,20 +30,29 @@ function checkForMatch() {
   console.log("Sorry, try again.");
 }
 }
-function flipCard(cardID) {
-	cardsInPlay.push(cards[cardID].rank);
+function flipCard() {
+	var cardID= this.getAttribute ("data-id")
 	checkForMatch()
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	this.setAttribute("src", cards[cardID].cardImage)
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		result = alert("You found a match!");
-	} else {
+	} else {cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1])
 		result = alert("Sorry, try again")
 	}
 	console.log("user flipped " + cards[cardID].rank)
+	cardsInPlay.push(cards[cardID].rank);
 	console.log(cards[cardID].cardImage)
 	console.log(cards[cardID].suit)
-
+}
+function createBoard (){
+for (var i = 0; i < cards.length; i++){
+	console.log(cards[i]);
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i)
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
 	}
-flipCard(0)
-flipCard(2)
-
+}
+createBoard ();
 
